2f3539d2211170e849e948cce6c4fd5fe8840dd5
Moemen Gaballah <moemengaballa@gmail.com>
2021-08-03 18:47:43 +0200

    init project with first test case

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    public function store()
+    {
+        return response(["message" => "Created"], 201);
+    }
+
+
+    
+}
---
+++ tests/Feature/BookManagementTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BookManagementTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    // public function test_example()
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
+
+    public function testStatus201WithMessageCreatedWhenCreateABook()
+    {
+        // $this->withoutExceptionHandling();
+
+        $response = $this->post("/books", [
+            "title" => "Gone with the wind",
+            "description" => "Bestseller or New York Times",
+            "author_id" => 1,
+            "ISBN" => "12b-422-24ff"
+        ]);
+
+        $response->assertCreated();
+        $response->assertJson(["message" => "Created"]);
+    }
+
+    public function testCountOfDatabaseInBooksTableIs1()
+    {
+        $this->post("/books", [
+            "title" => "Gone with the wind",
+            "description" => "Bestseller or New York Times",
+            "author_id" => 1,
+            "ISBN" => "12b-422-24ff"
+        ]);
+
+        $this->assertDatabaseCount('books', 1)
+    }
+
+}
